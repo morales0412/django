2331b0f5b90583f9c75994e76bd285a8fda6a665
morales04 <andres.4moscoso@gmail.com>
2026-03-24 13:11:49 -0500

    añadimiento filtro en templates

--- Libreria/views.py
+++ Libreria/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from datetime import datetime
 
 # request: es un objeto que contiene toda la información de la solicitud realizada por el cliente
 # HttpResponse("mensaje"): es una funcion que devuelve una respuesta HTTP con el mensaje especificado.
@@ -9,8 +10,10 @@ from django.shortcuts import redirect, render
 # def libros(request, nombre, autor): es una vista que recibe dos parametros, nombre y autor, a traves de la url, y dependiendo de lo que se pase en la url, se mostrara una respuesta diferente.
 def libros(request):
     datos_libros = {
-        "nombre": "Cien años de soledad",
+        "nombre": "Cien años de Soledad",
         "autor": "Gabriel García Márquez",
+        "fecha_hoy": datetime.now(),
+        "valor1": 50,
     }
     # render(request, "ruta_template.html", datos): es una funcion que renderiza un template con los datos especificados, sirve para mostrar una pagina web con los datos que se le pasaron, y se devuelve el html renderizado , como respuesta HTTP.
     return render(request, "libros/index.html", datos_libros)
